fd1c72f7d52778b1cdc1b6b5020cfe61b51ba4a1
Uddeshya <uddeshya98@gmail.com>
2021-08-04 12:23:10 +0530

    Add Humidity data to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
       const current = body.current;
       callback(
         undefined,
-        `${current.weather_descriptions[0]}. It is currently ${current.temperature} C out. It feels like ${current.feelslike} C.`
+        `${current.weather_descriptions[0]}. It is currently ${current.temperature} C out. It feels like ${current.feelslike} C. The humidity is ${current.humidity}.`
       );
     }
   });
